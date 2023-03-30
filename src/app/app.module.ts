bdfaa88401a858cfa63e21bdf0fff182f66cba5e
Tolga ÇELİK <tolga_ee_engineer@hotmail.com>
2023-03-30 22:02:53 +0100

    initial Side Nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Material section
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
     AppRoutingModule
   ],
   providers: [],
